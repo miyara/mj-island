7a78c32e1a1cb5087a3acf69631620d82a33bccf
miyara <miyara.masahide@gmail.com>
2023-08-11 19:11:59 +0900

    recreate

---
+++ mj-island/src/main/java/com/simantyu_engineer/mjisland/MjIslandApplication.java
@@ -0,0 +1,13 @@
+package com.simantyu_engineer.mjisland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjIslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjIslandApplication.class, args);
+	}
+
+}
---
+++ mj-island/src/test/java/com/simantyu_engineer/mjisland/MjIslandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simantyu_engineer.mjisland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjIslandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
