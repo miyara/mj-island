bb86d836c1bf541dd6f10a86362a5086d7913789
miyara <miyara.masahide@gmail.com>
2023-08-11 18:29:58 +0900

    create java project on spring boot

---
+++ src/main/java/com/mj_island/mjisland/MjIslandApplication.java
@@ -0,0 +1,13 @@
+package com.mj_island.mjisland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjIslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjIslandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mj_island/mjisland/MjIslandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj_island.mjisland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjIslandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
