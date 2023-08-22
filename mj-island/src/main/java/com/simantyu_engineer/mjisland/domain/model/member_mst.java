2fefebf3234b4fb55530df32f4f68dff7c9cb45c
miyara <miyara.masahide@gmail.com>
2023-08-22 22:36:04 +0900

    jdbc設定とlombokの利用設定を追記

---
+++ mj-island/src/main/java/com/simantyu_engineer/mjisland/domain/model/member_mst.java
@@ -0,0 +1,37 @@
+package com.simantyu_engineer.mjisland.domain.model;
+
+import com.google.appengine.repackaged.org.joda.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "member_mst")
+public class member_mst {
+    @Column(name = "member_id")
+    private String member_id;
+
+    @Column(name = "member_mail_address")
+    private String member_mail_address;
+
+    @Column(name = "member_password")
+    private String member_password;
+
+    @Column(name = "del_flg")
+    private String del_flg;
+
+    @Column(name = "create_user")
+    private String create_user;
+
+    @Column(name = "create_datetime")
+    private LocalDateTime create_datetime;
+
+    @Column(name = "update_user")
+    private String update_user;
+
+    @Column(name = "update_datetime")
+    private LocalDateTime update_datetime;
+}
