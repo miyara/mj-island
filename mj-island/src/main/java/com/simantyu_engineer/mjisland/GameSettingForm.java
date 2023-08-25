11b0f9f1ecb745c10c77dea4b932ac85473371a4
kazumaohama <110583522+kazumaohama@users.noreply.github.com>
2023-08-25 15:39:32 +0900

    xmlファイルにvalidationを追加 ゲーム設定関係で調整

---
+++ mj-island/src/main/java/com/simantyu_engineer/mjisland/GameSettingForm.java
@@ -0,0 +1,40 @@
+package com.simantyu_engineer.mjisland;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class GameSettingForm {
+
+    private Integer scoreId;
+
+    private Double  rating;
+
+    private Integer chip;
+
+    private Integer groupId;
+
+    private String  scoreTitle;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date    eventDate;
+
+    private String  location;
+
+    private String  comment;
+
+    private String  scoreNumTotal;
+
+    private String  createUser;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date    createDatetime;
+
+    private String  updateUser;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date    updateDatetime;
+}
---
+++ mj-island/src/main/java/com/simantyu_engineer/mjisland/GameSettingRepository.java
@@ -0,0 +1,7 @@
+package com.simantyu_engineer.mjisland;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameSettingRepository extends JpaRepository <GameSettingEntity, Long> {
+
+}
