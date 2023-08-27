bd0e9dfcd3b7707e0aa71f7a30884ecfc8ecc01b
miyara <miyara.masahide@gmail.com>
2023-08-27 22:16:40 +0900

    SCR005 DBの値を表示できるようになった版！

--- mj-island/src/main/java/com/simantyu_engineer/mjisland/domain/model/memberMst.java
+++ mj-island/src/main/java/com/simantyu_engineer/mjisland/domain/model/memberMst.java
@@ -1,11 +1,14 @@
 package com.simantyu_engineer.mjisland.domain.model;
 
-import com.google.appengine.repackaged.org.joda.time.LocalDateTime;
+// import com.google.appengine.repackaged.org.joda.time.LocalDateTime;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
 //import jakarta.persistence.MappedSuperclass;
 
@@ -31,11 +34,13 @@ public class memberMst {
     private String create_user;
 
     @Column(name = "create_datetime")
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime create_datetime;
 
     @Column(name = "update_user")
     private String update_user;
 
     @Column(name = "update_datetime")
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime update_datetime;
 }
