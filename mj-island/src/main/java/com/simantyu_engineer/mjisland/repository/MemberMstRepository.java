aeee4dab67e9ca3e3d1838e3ae1c224e43c0b433
miyara <miyara.masahide@gmail.com>
2023-09-09 10:58:25 +0900

    memberMstRepo minor edit

--- mj-island/src/main/java/com/simantyu_engineer/mjisland/repository/MemberMstRepository.java
+++ mj-island/src/main/java/com/simantyu_engineer/mjisland/repository/MemberMstRepository.java
@@ -1,10 +1,8 @@
 package com.simantyu_engineer.mjisland.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.simantyu_engineer.mjisland.domain.model.memberMst;
 
-@Repository
 public interface MemberMstRepository extends JpaRepository<memberMst, String>{
 }
