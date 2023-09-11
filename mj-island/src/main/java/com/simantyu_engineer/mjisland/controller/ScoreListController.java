ee65a10c00fbfb10ae6802af5de8189f6b740283
miyara <miyara.masahide@gmail.com>
2023-09-11 09:33:52 +0900

    SCR005 edit

--- mj-island/src/main/java/com/simantyu_engineer/mjisland/controller/ScoreListController.java
+++ mj-island/src/main/java/com/simantyu_engineer/mjisland/controller/ScoreListController.java
@@ -3,14 +3,17 @@ package com.simantyu_engineer.mjisland.controller;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.simantyu_engineer.mjisland.domain.model.memberMst;
+import com.simantyu_engineer.mjisland.domain.form.ScoreListForm;
 import com.simantyu_engineer.mjisland.service.ScoreListService;
 
 @Controller
@@ -19,24 +22,23 @@ public class ScoreListController {
     private ScoreListService service;
 
     @RequestMapping("/ScoreList")
-    public String SCR005ScoreList(Model model) {
+    public String SCR005ScoreList(Model model, 
+                                @ModelAttribute ScoreListForm scoreListForm) {
 
         // 初期設定項目の設定
         model.addAttribute("groupList", service.findAllGroupList());
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Date fromDate = new Date();
-        Date toDate = new Date();
-
-        try {
-            fromDate = sdf.parse("2023-08-30");
-            toDate = sdf.parse("2023-08-31");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        model.addAttribute("scoreList", service.findScoreList(fromDate, toDate, "GRP0000001"));
+        // 検索結果を設定
+        model.addAttribute("scoreList", service.findScoreList(scoreListForm.getEventDateFrom(), 
+                                                                            scoreListForm.getEventDateTo(),
+                                                                            scoreListForm.getGroupId()));
 
         return "SCR005scoreList";
+    }
+
+    // 未入力の項目をすべてNullに変換してくれる便利関数
+    @InitBinder
+    public void initbinder(WebDataBinder binder){
+           binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 }
